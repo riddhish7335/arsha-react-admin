d6c542d3d531352c7b18c720e56b56a804411dd8
rkparmar7335 <parmar.riddhish@gmail.com>
2024-06-23 01:01:38 +0530

    change team lable

--- src/Components/OurTeam.js
+++ src/Components/OurTeam.js
@@ -8,7 +8,7 @@ const OurTeam = () => {
       <div className="container" data-aos="fade-up">
 
         <div className="section-title">
-          <h2>Team</h2>
+          <h2>Our Team</h2>
           <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
         </div>
 
